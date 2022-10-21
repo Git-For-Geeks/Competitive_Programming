d04f365abcffff1350ae440cf082c271794f0043
Nishantth1 <97899868+Nishantth1@users.noreply.github.com>
2022-10-21 17:10:31 +0530

    Added Solution for Valid Number

--- Valid Number/Nishant.java
+++ Valid Number/Nishant.java
@@ -3,7 +3,7 @@ class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String regex = "^[-+]?\d*(([.]+\d+)+)?(\d+[.])?([eE][-+]?\d+)?$";
-        String number = "+9322.3e10";
+        String number = sc.nextLine();
         if(number.matches(regex)){
             System.out.println("true");
         }
